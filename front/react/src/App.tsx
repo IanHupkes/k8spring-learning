db7aa4927b8474aa5ba77904be78de25139f995f
IanHupkes <ian.hupkes@capgemini.com>
2026-05-20 16:47:18 +0200

    git npm ci fixes

--- front/react/src/App.tsx
+++ front/react/src/App.tsx
@@ -36,11 +36,12 @@ export default function UsersApp() {
 
 
     useEffect(() => {
-        const loadUsers = async () => {
-            const users = await api.getUsers(); // example
-            setUsers(users); // ✅ now inside async flow
-        };
-        loadUsers();
+        fetchUsers();
+//         const loadUsers = async () => {
+//             const users = await api.getUsers(); // example
+//             setUsers(users); // ✅ now inside async flow
+//         };
+//         loadUsers();
     }, []);
 
     const addUser = async (e: React.FormEvent<HTMLFormElement>): Promise<void> => {
--- front/react/vite.config.ts
+++ front/react/vite.config.ts
@@ -1,8 +1,9 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from "path";
-import tsconfigPaths from "vite-tsconfig-paths";
+import tsconfigPaths from "vite-tsconfig-paths
+
 
 // https://vite.dev/config/
 
