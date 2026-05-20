dc04334c3b23eea1059299153c42fcfabcab51d0
IanHupkes <ian.hupkes@capgemini.com>
2026-05-20 17:22:40 +0200

    added CITY to USER model

--- back/spring/src/main/java/com/example/k8spring/spring_api/model/User.java
+++ back/spring/src/main/java/com/example/k8spring/spring_api/model/User.java
@@ -12,6 +12,7 @@ public class User {
 
     private String name;
     private String email;
+    private String city;
 
     public User() {
     }
@@ -33,11 +34,19 @@ public class User {
         return email;
     }
 
+    public String getCity() {
+        return city;
+    }
+
     public void setName(String name) {
         this.name = name;
     }
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
     }
 }
