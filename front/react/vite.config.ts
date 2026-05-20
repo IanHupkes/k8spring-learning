97629b74bf2a6aeb9cf712525d1f0e673a45b1a4
IanHupkes <ian.hupkes@capgemini.com>
2026-05-20 16:48:51 +0200

    typo

--- front/react/vite.config.ts
+++ front/react/vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from "path";
-import tsconfigPaths from "vite-tsconfig-paths
+import tsconfigPaths from "vite-tsconfig-path";
 
 
 // https://vite.dev/config/
