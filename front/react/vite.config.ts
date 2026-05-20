b0ca09ba4bbc4e58fe2ee031222dfffcf8a7c76b
IanHupkes <ian.hupkes@capgemini.com>
2026-05-20 17:00:26 +0200

    import tsconfigPaths from "vite-tsconfig-paths";

--- front/react/vite.config.ts
+++ front/react/vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from "path";
-import tsconfigPaths from "vite-tsconfig-path";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 
 // https://vite.dev/config/
